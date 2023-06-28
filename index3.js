255c75170e96119a04e5fb36ad71e53a78008370
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-06-28 20:10:23 -0300

    Aula Intermediaria

---
+++ index3.js
@@ -0,0 +1,37 @@
+// let nomes = ["Mariah", "Marcelo", "Alessandra", "Gabriel"]
+// let idades = [22,25,26,18]
+
+// let pessoa = ["Everton", 25,"Ensino Superior Complesto"]
+
+// console.log(`Nome: ${pessoa[0]}`); 
+// console.log(`Idade: ${pessoa[1]}`); 
+
+
+
+
+// for (let index = 0; index < 4; index++) {
+//     console.log(`Valor do index: ${index}`);
+//     console.log(nomes[index]);
+//     console.log(idades[index]);
+//     console.log("---------------");
+// }
+
+// let numeros = []
+
+// for (let index = 0; index < 5; index++) {
+//     numeros[index] = prompt("Digite um numero")
+// }
+
+// for (let index = 0; index < numeros.length; index++) {
+//     console.log(`O numero do usuario é ${numeros[index]}`);
+// }
+
+let nomes = ["Mariah", "Marcelo", "Alessandra", "Gabriel"]
+
+let sobrenome = "Figueiredo"
+
+console.log(sobrenome.length);
+
+for (let index = 0; index < nomes.length; index++) {
+    console.log(`Seu nome é: ${nomes[nomes.length - 1]}`);    
+}
