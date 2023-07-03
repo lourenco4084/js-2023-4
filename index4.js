5fad7b0e2cf7e92931d2742bb4f8d59b2aeafc2f
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-07-03 20:09:10 -0300

    Atualizando Funções

---
+++ index4.js
@@ -0,0 +1,41 @@
+// function exibirNome(nome = "") {
+//   let frase = `Seu nome é ${nome}`
+//   return frase
+// }
+
+// let nome = prompt("Digite seu nome: ")
+// let texto = exibirNome(nome)
+// console.log(texto);
+
+let carrinho = {
+    itens: [
+        {
+            nome: "Lapis",
+            qtd: 2,
+            preco: 120
+        },
+        {
+            nome: "Borracha",
+            qtd: 1,
+            preco: 80
+        },
+        {
+            nome: "Caneta",
+            qtd: 5,
+            preco: 80
+        },
+    ]
+}
+
+function somarQtd() {
+    let totalQtd = 0
+
+    for (let index = 0; index < carrinho.itens.length; index++) {
+        totalQtd += carrinho.itens[index].qtd
+    }
+
+    return totalQtd
+}
+
+
+
