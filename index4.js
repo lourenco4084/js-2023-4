b1055371fefd805c2b5fe6d43c4d54f379fe8688
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-07-05 20:05:29 -0300

    Revisão Funções/Arrays

--- index4.js
+++ index4.js
@@ -7,25 +7,7 @@
 // let texto = exibirNome(nome)
 // console.log(texto);
 
-let carrinho = {
-    itens: [
-        {
-            nome: "Lapis",
-            qtd: 2,
-            preco: 120
-        },
-        {
-            nome: "Borracha",
-            qtd: 1,
-            preco: 80
-        },
-        {
-            nome: "Caneta",
-            qtd: 5,
-            preco: 80
-        },
-    ]
-}
+
 
 function somarQtd() {
     let totalQtd = 0
@@ -43,8 +25,61 @@ function somarPreco() {
     for (let index = 0; index < carrinho.itens.length; index++) {
         totalPreco += carrinho.itens[index].qtd * carrinho.itens[index].preco
     }
-
+    carrinho.total = totalPreco
     return totalPreco
 }
 
+function addItem(nome = "", qtd = 0, preco = 0) {
+    carrinho.itens.push({ nome, qtd, preco })
+}
 
+// let numeros = []
+
+// numeros.push(4)
+// numeros.push(8)
+
+// console.log(numeros);
+
+// numeros.pop()
+
+// console.log(numeros);
+
+// numeros.unshift(20)
+
+// console.log(numeros);
+
+// numeros.shift()
+
+// console.log(numeros);
+let carrinho = {
+    itens: [
+        {
+            nome: "borracha",
+            qtd: 5,
+            preco: 20
+        },
+        {
+            nome: "Lapis",
+            qtd: 1,
+            preco: 110
+        },
+        {
+            nome: "Caneta",
+            qtd: 5,
+            preco: 70
+        },
+    ],
+    total: 0
+}
+
+let precoTotal = 0
+
+carrinho.itens.map(produto => {
+    precoTotal += produto.qtd * produto.preco
+})
+
+let filtro = carrinho.itens.filter(produto => produto.nome == "Lapis")
+
+let find = carrinho.itens.find(produto => produto.preco < 100 && produto.qtd == 5)
+
+console.log(find);
---
+++ index5.js
@@ -0,0 +1,39 @@
+// let pessoas = [
+//     {
+//         nome: "Pedro",
+//         idade: 22,
+//         profissao: "Dev Junior",
+//         email: "pedro@gmail.com",
+//         cnh: true
+//     },
+// ]
+
+// function visualizarPessoas() {
+//     pessoas.map(pessoa => {
+//         console.log(pessoa);
+//     })
+// }
+
+// function addPessoa(nome = "", idade = 0, profissao = "", email = "", cnh = false) {
+//     let pessoa = {
+//         nome,
+//         idade,
+//         profissao,
+//         email,
+//         cnh
+//     }
+
+//     pessoas.push(pessoa)
+//     visualizarPessoas()
+// }
+
+// // Retorna a pessoa com idade menor que 20
+// function idadeMin() {
+//     let pessoasIdade = pessoas.filter(pessoa => pessoa.idade < 20)
+
+//     return pessoasIdade
+// }
+
+// function filtrarPorNome(nome) {
+//     return pessoas.find(pessoa => pessoa.nome === nome);
+// }
